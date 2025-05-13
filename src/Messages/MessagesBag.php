046d5c7e6f1ab7fd5e9b1fe3e2359b08a3101a21
Gean Pedro da Silva <36330402+HimAndRobot@users.noreply.github.com>
2025-05-13 14:01:10 -0300

    🔧 refactor: Update message ordering logic in MessagesBag
    - Modified the assignment of `$this->messages` to conditionally reverse the order based on the specified `$order` parameter, enhancing the flexibility of message retrieval.

--- src/Messages/MessagesBag.php
+++ src/Messages/MessagesBag.php
@@ -73,7 +73,7 @@ class MessagesBag
                 break;
             }
         }
-        $this->messages = $newMessages;
+        $this->messages = $order === 'desc' ? array_reverse($newMessages) : $newMessages;
         return $this;
     }
 
